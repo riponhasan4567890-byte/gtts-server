3f2378b3b2fba3f49939cface80d3ae08c3afc67
riponhasan4567890-byte <riponhasan4567890@gmail.com>
2026-05-16 22:28:58 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, send_file
+from gtts import gTTS
+import io
+
+app = Flask(__name__)
+
+@app.route('/tts', methods=['GET'])
+def tts():
+    text = request.args.get('text', 'hello')
+    lang = request.args.get('lang', 'bn')
+    tts = gTTS(text=text, lang=lang)
+    mp3_fp = io.BytesIO()
+    tts.write_to_fp(mp3_fp)
+    mp3_fp.seek(0)
+    return send_file(mp3_fp, mimetype='audio/mpeg')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
